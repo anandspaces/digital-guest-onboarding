f852d2a1beee5bdd1e000baa83f6d24285b904f3
anand <anandvaibhaw000@gmail.com>
2025-01-27 23:08:28 +0530

    fixed env variables

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
+console.log(process.env.MONGO_URI)
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
